0cdcabf56f3a174c9b09bb906e9ddf39b7abe5cf
yspwf <846163966@qq.com>
2018-11-17 16:50:59 +0800

    http

--- src/http.php
+++ src/http.php
@@ -2,16 +2,22 @@
 class http{
 
     public $http;
+    public $app;
 
     public function __construct($port){
         $this->http = new swoole_http_server('0.0.0.0', $port);
         $this->http->on('start', [$this, 'onStart']);
+        $this->http->on('workStart', [$this. 'onWorkStart']);
         $this->http->on('request', [$this, 'onRequest']);
         $this->http->start();
     }
 
     public function onStart($server){
         echo "this request is start";
+    }
+
+    public function onWorkStart(){
+        $this->app = 1;
     }
 
     public function onRequest($request, $response){
