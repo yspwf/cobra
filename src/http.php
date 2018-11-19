bd512e7b811223cf4ee14439cd24e5835fcfff79
846163966@qq.com <846163966@qq.com>
2018-11-19 23:26:36 +0800

    wer

--- src/http.php
+++ src/http.php
@@ -7,6 +7,7 @@ class Http{
 	public function __construct($port){
 		$this->server = new swoole_http_server('0.0.0.0', $port);
 		$this->server->on('start', [$this, 'onStart']);
+		$this->server->on('WorkerStart', [$this, 'onWorkerStart']);
 		$this->server->on('request', [$this, 'onRequest']);
 	}
 
@@ -14,9 +15,18 @@ class Http{
 		echo "server is tart \r\n";
 	}
 
+	public function onWorkerStart($server){
+		spl_autoload_register([$this, 'autoloader']);
+	}
+
 	public function onRequest($request, $response){
-		$response->header('Content-type',"text/plain");
-		$response->end("hello");
+		$obj = new \Article();
+		//$response->header('Content-type',"text/plain");
+		//$response->end("hello");
+	}
+
+	public function autoloader($class){
+		echo $class;
 	}
 
 	public function run(){
