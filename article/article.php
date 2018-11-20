e1c6658b670861fe2125acef2e428f034e55c766
yspwf <846163966@qq.com>
2018-11-20 10:18:13 +0800

    23345asdf

--- article/article.php
+++ article/article.php
@@ -1,11 +1,12 @@
 <?php 
 namespace article;
+use article\views;
 
 class article{
 
     public function index(){
 
-        $views = new \views();
+        $views = new views();
         $views->show();
         echo "hello  article";
        
