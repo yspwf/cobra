d2cfc25f8ee18e6234710812f2e7f577e33a2ab3
yspwf <846163966@qq.com>
2018-11-20 11:12:36 +0800

    234sdfg

--- article/article.php
+++ article/article.php
@@ -8,13 +8,13 @@ class article{
 
     public function index(){
 
-        $views = new views();
-        $views->show();
+        // $views = new views();
+        // $views->show();
         echo "\r\n";
         $db = new db();
         $db->query();
         echo "\r\n";
-        echo "hello  article";
+        // echo "hello  article";
        
 
     }
--- src/db.php
+++ src/db.php
@@ -5,12 +5,17 @@ class db{
 
     public $connection = null;
 
+
+    public function __construct(){
+        $this->connection = $this->getConnection();
+    }
+
     public function getConnection(){
         $dsn = 'mysql:dbname=demo;host=localhost,port=3306';
         $user = 'root';
         $pwd = '123456';
         try{
-            $this->connection = new \PDO($dsn, $user, $pwd);
+            return new \PDO($dsn, $user, $pwd);
         }catch(PDOException $e){
             var_dump($e->getMessage());
         }
