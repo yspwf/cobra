d8cae5cb77f1cce304b8b286fbe1a3643f1e6510
yspwf <846163966@qq.com>
2018-11-20 09:51:42 +0800

    234345

--- src/autoload.php
+++ src/autoload.php
@@ -1,12 +1,43 @@
 <?php 
 
-class autoload{
 
-    public function __construct(){
-        echo "234234234234234234234234234";
+
+function loader($classname){
+    echo $classname;
+    $baseClasspath = \str_replace('\\', DIRECTORY_SEPARATOR, $classname) . '.php';
+    // 如果文件存在, 引用文件
+    $classpath = __DIR__ . DIRECTORY_SEPARATOR ."..". DIRECTORY_SEPARATOR . $baseClasspath;
+    //echo $classpath."\r\n";
+    if (\is_file($classpath)) {
+        require "{$classpath}";
+        return;
     }
 
+        // $filename = ROOT."/".$classname.".php";
+        // $filename = str_replace("\\","/",$filename);
+        // echo $filename."\r\n";
+        // if(!file_exists($filename)){
+        //     //header("HTTP/1.1 404 Not Found");
+        //     echo "404";
+        //     exit();
+        // }
+        // require $filename;
+
+
+        // $filename = ROOT."/".$classname.".php";
+        //  // 构建文件名, 将namespace中的 '\' 替换为文件系统的分隔符 '/'
+        // $baseClasspath = \str_replace('\\', DIRECTORY_SEPARATOR, $filename);
+        // echo $baseClasspath."\r\n";
+        // // 如果文件存在, 引用文件
+        // $classpath = __DIR__ . DIRECTORY_SEPARATOR . $baseClasspath;
+        // if (\is_file($classpath)) {
+        //     require "{$classpath}";
+        //     return;
+        // }
+ 
 }
+
+spl_autoload_register('loader');
 
 
 
