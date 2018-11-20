1e81bf40f80dc3856e429af0221400f306cd9af7
yspwf <846163966@qq.com>
2018-11-20 11:20:16 +0800

    23retwert

--- src/db.php
+++ src/db.php
@@ -7,20 +7,31 @@ class db{
 
 
     public function __construct(){
-        $this->connection = $this->getConnection();
+        try {
+            $this->connection = new \PDO('mysql:host=localhost;dbname=demo;port=3306', $user, $pass); //初始化一个PDO对象
+            echo "连接成功<br/>";
+            /*你还可以进行一次搜索操作
+            foreach ($dbh->query('SELECT * from FOO') as $row) {
+                print_r($row); //你可以用 echo($GLOBAL); 来看到这些值
+            }
+            */
+            $dbh = null;
+        } catch (PDOException $e) {
+            die ("Error!: " . $e->getMessage() . "<br/>");
+        }
     }
 
-    public function getConnection(){
-        $dsn = 'mysql:dbname=demo;host=localhost,port=3306';
-        $user = 'root';
-        $pwd = '123456';
-        try{
-            $conn = new \PDO($dsn, $user, $pwd);
-        }catch(PDOException $e){
-            var_dump($e->getMessage());
-        }
-        return $conn;
-    }
+    // public function getConnection(){
+    //     $dsn = 'mysql:dbname=demo;host=localhost,port=3306';
+    //     $user = 'root';
+    //     $pwd = '123456';
+    //     try{
+    //         $conn = new \PDO($dsn, $user, $pwd);
+    //     }catch(PDOException $e){
+    //         var_dump($e->getMessage());
+    //     }
+    //     return $conn;
+    // }
 
     public function query(){
         var_dump($this->connection->query('select 1+1'));
