b8f3462b437a2387637531b92d62afa5d4fc42b4
846163966@qq.com <846163966@qq.com>
2018-11-24 21:14:43 +0800

    dsaf

--- article/article.php
+++ article/article.php
@@ -1,21 +1,18 @@
 <?php 
 namespace article;
 
-use article\views;
-use app\db;
-
 class article{
 
     public function index(){
 
         // $views = new views();
         // $views->show();
-        echo "\r\n";
-        $db = new \app\db();
+        //echo "\r\n";
+        //$db = new \app\db();
     //     $result = $db->query();
     //    var_dump($result);
-        echo "\r\n";
-        // echo "hello  article";
+        //SSSecho "\r\n";
+        echo "hello  article";
        
 
     }
