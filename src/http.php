24edd4158c579b3cae7e1231e7461558cde02507
846163966@qq.com <846163966@qq.com>
2018-11-24 20:56:34 +0800

    r34er

--- src/http.php
+++ src/http.php
@@ -24,10 +24,12 @@ class http{
 
 	public function onRequest($request, $response){
 		$uri = $request->server['path_info'];
-		var_dump($uri);
-		var_dump(explode("/", $uri));
-		//list($module,$controller,$action) = implode("/", $uri);
-		//echo $module."----".$controller."-----".$action;
+		//var_dump($uri);
+		$uri = explode("/", $uri);
+		array_shift($uri);
+		list($module,$controller,$action) = $uri;
+
+		echo $module."----".$controller."-----".$action;
 
 		//$url = new request();
 		//$url->url($request, $response);
