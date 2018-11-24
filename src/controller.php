9ed4e7cd19cf1d2bf10a09976f92c43d45926238
846163966@qq.com <846163966@qq.com>
2018-11-24 21:43:31 +0800

    contro

--- src/controller.php
+++ src/controller.php
@@ -6,6 +6,11 @@ class controller{
 	public $request = '';
 	public $response = '';
 
+	public function __construct($request, $response){
+		$this->request = $request;
+		$this->response = $response;
+	}
+
 	public function render(){
 		$this->response->header('Content-type','text/html');
 
