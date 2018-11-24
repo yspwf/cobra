67ac94182d53dbfd6c135a0801f24c4de0b2bdc4
846163966@qq.com <846163966@qq.com>
2018-11-24 18:48:55 +0800

    345453

--- src/http.php
+++ src/http.php
@@ -24,7 +24,7 @@ class http{
 
 	public function onRequest($request, $response){
 		$uri = $request->server['path_info'];
-		//var_dump($uri);
+		var_dump($uri);
 		list($module,$controller,$action) = implode("/", $uri);
 		echo $module."----".$controller."-----".$action;
 
