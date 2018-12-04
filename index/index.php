0295019ddb52559e73c7052910a4d2536304a7ad
yspwf <846163966@qq.com>
2018-12-04 16:52:08 +0800

    add index

---
+++ index/index.php
@@ -0,0 +1,27 @@
+<?php 
+namespace index;
+use app\controller;
+
+class index extends controller{
+
+    public function index(){
+
+        // $views = new views();
+        // $views->show();
+        //echo "\r\n";
+        //$db = new \app\db();
+    //     $result = $db->query();
+    //    var_dump($result);
+        //SSSecho "\r\n";
+        $this->render();
+        echo "hello  article";
+       
+
+    }
+
+    
+
+}
+
+
+?>
