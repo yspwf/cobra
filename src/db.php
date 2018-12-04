edc2754e8cf88ecc6fe055eaf37d69dd305ea50a
yspwf <846163966@qq.com>
2018-12-04 18:30:36 +0800

    connect query

--- src/db.php
+++ src/db.php
@@ -4,7 +4,9 @@ namespace app;
 class db extends Connection{
 
     public function query(){
-        echo "mysql   Model";
+        $result = $this->query();
+        var_dump($result);
+        //echo "mysql   Model";
     }
 
 }
